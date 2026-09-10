fc732869bf7e099a667e0ab1295d4bdd1c31dc05
pxoqxo <pxoqxo@atomicmail.io>
2026-09-10 20:36:25 +0530

    Application_Startup event added.

--- MakeVideo/App.xaml.cs
+++ MakeVideo/App.xaml.cs
@@ -4,6 +4,10 @@ namespace Pxoqxo.MakeVideo
 {
     public partial class App : Application
     {
-
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
     }
 }
